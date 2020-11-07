a5b60d377bb6198c65941a727bfff7cdcf7ba561
Duc Minh <ducle7144@gmail.com>
2020-11-07 09:42:49 +0700

    Layout Setup

---
+++ app/src/main/java/com/example/videomeet/activities/SignInActivity.java
@@ -0,0 +1,17 @@
+package com.example.videomeet.activities;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import com.example.videomeet.R;
+
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        findViewById(R.id.textSignUp).setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+    }
+}
